6b1be9d8848020c9736979253880d9515bb6df99
CodeM0nkeyc <vangazs35@gmail.com>
2025-10-27 19:22:00 +0200

    Implementin EmailService

--- TourApp.Application/Models/Email/EmailMessage.cs
+++ TourApp.Application/Models/Email/EmailMessage.cs
@@ -1,8 +1,13 @@
 ﻿namespace TourApp.Application.Models.Email;
 
-public record EmailMessage(
-    string From,
-    string To,
-    string? Subject,
-    string Body
-);
+public record EmailMessage
+{
+    public string FromAddress { get; init; }
+    public string FromName { get; init; }
+    
+    public string ToAddress { get; init; }
+    public string ToName { get; init; }
+    
+    public string? Subject { get; init; }
+    public string Body { get; init; }
+}
--- TourApp.Infrastructure/Services/Email/EmailService.cs
+++ TourApp.Infrastructure/Services/Email/EmailService.cs
@@ -1,9 +1,34 @@
-﻿namespace TourApp.Infrastructure.Services.Email;
+﻿using MailKit.Security;
+
+namespace TourApp.Infrastructure.Services.Email;
 
 public class EmailService : IEmailService
 {
-    public Task SendEmailAsync(EmailMessage message)
+    private readonly SmtpSettings _smtpSettings;
+
+    public EmailService(IOptions<SmtpSettings> smtpSettings)
     {
-        throw new NotImplementedException();
+        _smtpSettings = smtpSettings.Value;
+    }
+    
+    public async Task SendEmailAsync(EmailMessage message)
+    {
+        MimeMessage emailMessage = new MimeMessage()
+        {
+            From = { new MailboxAddress(message.FromName, message.FromAddress) },
+            To = { new MailboxAddress(message.ToName, message.ToAddress) },
+            Subject = message.Subject,
+            Body = new TextPart(TextFormat.Plain)
+            {
+                Text = message.Body
+            }
+        };
+
+        using SmtpClient client = new SmtpClient();
+        
+        await client.ConnectAsync(_smtpSettings.Host, _smtpSettings.Port, SecureSocketOptions.StartTls);
+        await client.AuthenticateAsync(_smtpSettings.Username, _smtpSettings.Password);
+        await client.SendAsync(emailMessage);
+        await client.DisconnectAsync(true);
     }
 }
