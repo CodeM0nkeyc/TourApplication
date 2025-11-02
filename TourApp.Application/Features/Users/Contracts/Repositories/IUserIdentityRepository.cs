9bcb8880cb2ed053ec8235c0ad46fe0e6dda48ef
CodeM0nkeyc <vangazs35@gmail.com>
2025-11-02 15:21:54 +0200

    Repository changes

---
+++ TourApp.Application/Features/Users/Contracts/Repositories/IUserIdentityRepository.cs
@@ -0,0 +1,16 @@
+﻿using TourApp.Application.Contracts.Repositories;
+
+namespace TourApp.Application.Features.Users.Contracts.Repositories;
+
+public interface IUserIdentityRepository : IUnitOfWork
+{
+    public Task<UserIdentity?> GetByIdAsync(int id);
+    public Task<UserIdentity?> GetByEmailAsync(string email);
+
+    public Task<bool> ExistsAsync(string email);
+    
+    public void Update(UserIdentity userIdentity);
+    public void Update(UserIdentity userIdentity, params string[] props);
+    
+    public void SetConfirmationCode(UserIdentity userIdentity, ConfirmationCode? confirmationCode);
+}
---
+++ TourApp.Persistence/Repositories/UserIdentityRepository.cs
@@ -0,0 +1,64 @@
+﻿namespace TourApp.Persistence.Repositories;
+
+public class UserIdentityRepository : IUserIdentityRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+    private readonly IGenericRepository<UserIdentity> _genericRepository;
+
+    public UserIdentityRepository(
+        ApplicationDbContext dbContext, IGenericRepository<UserIdentity> genericRepository)
+    {
+        _dbContext = dbContext;
+        _genericRepository = genericRepository;
+        _dbContext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+    }
+    
+    public async Task<UserIdentity?> GetByIdAsync(int id)
+    {
+        UserIdentity? userIdentity = await _dbContext.UserIdentities
+            .FirstOrDefaultAsync(identity => identity.Id == id);
+
+        return userIdentity;
+    }
+
+    public async Task<UserIdentity?> GetByEmailAsync(string email)
+    {
+        UserIdentity? userIdentity = await _dbContext.UserIdentities
+            .FirstOrDefaultAsync(identity => identity.Email == email);
+
+        return userIdentity;
+    }
+
+    public async Task<bool> ExistsAsync(string email)
+    {
+        bool exists = await _dbContext.UserIdentities.AnyAsync(identity => identity.Email == email);
+        
+        return exists;
+    }
+
+    public void Update(UserIdentity userIdentity)
+    {
+        _genericRepository.Update(userIdentity);
+    }
+
+
+    public void Update(UserIdentity userIdentity, params string[] props)
+    {
+        _genericRepository.Update(userIdentity, props);
+    }
+
+    public void SetConfirmationCode(UserIdentity userIdentity, ConfirmationCode? confirmationCode)
+    {
+        if (_dbContext.Entry(userIdentity).State == EntityState.Detached)
+        {
+            _dbContext.Attach(userIdentity);
+        }
+        
+        userIdentity.ConfirmationCode = confirmationCode;
+    }
+
+    public async Task SaveAsync()
+    {
+        await _dbContext.SaveChangesAsync();
+    }
+}
